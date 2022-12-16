856c5d1bed41c94a322570f3a84cbc8fa8968056
Jack Scarpelli <Festivetomcat99@gmail.com>
2022-12-15 22:10:25 -0500

    commit tree eab44de6d69770b2cc6bbbb33cdaf13e6e11c962

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("go to the beach")); // "This Saturday, I want to go to the beach!"
+
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+
+  function wrapAdjective(wrapper) {
+    return function(adjective) {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
+  const highlight = wrapAdjective('*');
+  console.log(highlight('a hard worker')); // "You are *a hard worker*!"
+  
+  const emphasize = wrapAdjective('||');
+  console.log(emphasize('a dedicated programmer')); // "You are ||a dedicated programmer||!"
+  
